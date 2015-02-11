bd9e14d07440c1bc1715a82a038df5ffd8a8fed3
zend <combbs@gmail.com>
2015-02-11 19:17:09 +0800

    using package library

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-src/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world.")
+	fmt.Printf("\n")
+	fmt.Printf(stringutil.Reverse("Hello, world."))
+	fmt.Printf("\n")
 }
